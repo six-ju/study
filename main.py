d2c910c5831921e861ed4cd96b6dfbbadaf72715
yookjunho <yuk1479@naver.com>
2022-12-01 20:46:16 +0900

    공부한 자료

---
+++ main.py
@@ -0,0 +1,17 @@
+# from flask import Flask , render_template
+#
+# app = Flask(__name__)
+#
+# @app.route('/')
+# def hello():
+#     return render_template("index.html")
+#
+# if __name__ == "__main__":
+#     app.run()
+
+
+def price(name , wage = 9000,time):
+
+
+name = input("이름을 입력하세요 :")
+time = input("몇시간일했나요 :")
---
+++ pay_cal.py
@@ -0,0 +1,7 @@
+def price(name,time):
+    wage = 9650
+    result = wage*time
+    print(f"{name}은 {time}시간 일했으므로 월급은 {result}입니다")
+    return result
+
+price(name=input("이름"),time=int(input("시간")))
