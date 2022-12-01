b93629643ccf0be35a1a5d3f73e9ae02c2f5704c
yookjunho <yuk1479@naver.com>
2022-12-01 20:47:59 +0900

    no message

--- pay_cal.py
+++ pay_cal.py
@@ -4,4 +4,4 @@ def price(name,time):
     print(f"{name}은 {time}시간 일했으므로 월급은 {result}입니다")
     return result
 
-price(name=input("이름"),time=int(input("시간")))
+price(name=input("이름"),time=int(input("시간"))) 
